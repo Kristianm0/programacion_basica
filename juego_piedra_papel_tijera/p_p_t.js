9084bb8320fc1f499134c5a927313a4ef03ec977
krism <krisjaviermartinez@gmail.com>
2023-07-07 10:11:09 -0500

    change language

--- juego_piedra_papel_tijera/p_p_t.js
+++ juego_piedra_papel_tijera/p_p_t.js
@@ -1,18 +1,16 @@
-// 1 es papel. 2 es piedra. 3 es tijera
 
-//Creamos unas variables
+//Create the vars
 let jugador = 0
 let pc = 2
 
-// le pedimos al usuario que eliga una opcion apartir de un numero 
-jugador = prompt("Elige 1 tijera, 2 papel, 3 tijera")
-//alert("Elige "+jugador)
+// ask the values for user
+jugador = prompt("Chose 1 rock, 2 paper, 3 scissor")
 
-// Usamos unas condicionales
+// Conditions
 if(jugador == 1){
-    alert("Elegiste piedra")
+    alert("You chose rock")
 } else if (jugador == 2){
-    alert("Elegistes papel")
+    alert("You chose paper")
 } else if (jugador == 3){
-    alert("Elegistes tijera")
+    alert("You chose scissor")
 }
